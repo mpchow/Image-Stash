0e8b78faa9f5712e8cf0c49fafac0e75af3d9a62
mpchow <mattchow918@gmail.com>
2021-01-02 00:22:30 -0800

    Testing endpoint

--- server/routes/images.js
+++ server/routes/images.js
@@ -3,48 +3,71 @@ var router = express.Router();
 
 const imageDB = require('../modules/db').Image;
 
+const getImgFile = (url, filename) => {
+  let arr = url.split(','),
+      mime = arr[0].match(/:(.*?);/)[1],
+      bstr = atob(arr[1]), 
+      n = bstr.length, 
+      u8arr = new Uint8Array(n);
+      
+  while(n--){
+      u8arr[n] = bstr.charCodeAt(n);
+  }
+  
+  return new File([u8arr], filename, {type:mime});
+}
+
 
 router.get('/', function(req, res, next) {
-  const param = req.body;
-  imageDB.find({}, function(err, images) {
+  const param = req;
+  imageDB.find({userEmail: param.email}, function(err, images) {
     if(err) {
       throw new Error(err);
     }
 
-    res.send('');
+    res.send({msg: "Success", images: images});
   });
 
 });
 
 router.post('/', function(req, res, next) {
   const param = req.body;
-  const image = new imageDB(param);
+  const images = param.images;
 
-  imageDB.findOne({path: param.path}, function(err, image) {
-    if(err) {
-      next(err);
-    }
-    if (image !== null) {
-      res.send({msg: "Image already in db"})
-    }
-  });
+  for (let image in images) {
+    imageDB.findOne({userEmail: param.email, name: image.name}, function(err, image) {
+      if(err) {
+        next(err);
+      }
+      // if (image !== null) {
+      //   res.send({msg: "Image already in db"});
+      // }
+    });
 
-  image.save(function(err) {
-    if(err) {
-      next(err);
-    }
-  });
+    console.log(getImgFile(image.base64, image.name));
+
+    //GET the s3 path here
+
+    const imageObj = new imageDB({name: image.name, userEmail: param.email, path: "Test"});
+
+
+    imageObj.save(function(err) {
+      if(err) {
+        next(err);
+      }
+    });
+  }
 
   res.send({msg: "Success"});
 });
 
 router.delete('/', function(req, res, next) {
   const param = req.body;
-  imageDB.create({}, function(err, image) {
+  imageDB.create({userEmail: param.email, name: param.name}, function(err, image) {
     if(err) {
       throw new Error(err);
     }
-    res.send({msg: ""});
+    res.send({msg: "Success"});
   })
 });
 
