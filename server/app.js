bc70e9206fc6cae8d632d261ce8870c8a8c57a56
mpchow <mattchow918@gmail.com>
2021-01-02 01:13:20 -0800

    Increase payload capacity

--- server/app.js
+++ server/app.js
@@ -18,6 +18,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.bodyParser({limit: '50mb'}));
 
 app.use('/images', imagesRouter);
 app.use('/users', usersRouter);
